5632e04c0e60049329e0a6efaf5275fab52000c9
Paul Drangeid <pdrangeid@gmail.com>
2019-10-12 12:59:29 -0500

    graph-commit

---
+++ set-regcredentials.ps1
@@ -0,0 +1,61 @@
+<# 
+.SYNOPSIS 
+ Configure credentials and (secure-string) store them in the registry.
+ 
+.DESCRIPTION 
+ This script must be run as the account that will be used to run tasks that connect and query
+ to resources that need credentials. The password is converted into a securestring object and stored in the registry,
+ and only retreivable by the same (user) account.
+
+  ***Please be aware, securestring storage is only as secure as the machine and users operating
+ (or with access to) it. If you have access to the scripts, and some level of local administrative privilleges,
+ it is a trivial task to alter the scripts in order to recover/retrieve the original text of the stored
+ securestring values.  As best security practices demand, these stored credentials should only provide the least
+ privillege required to accomplish the task.
+
+ Any processes that store/retrieve these values should ONLY be stored and run on a secured and limited-access
+ endpoint, with a secured service account.
+
+ 
+.NOTES 
+┌─────────────────────────────────────────────────────────────────────────────────────────────┐ 
+│ set-regcredentials.ps1                                                                      │ 
+├─────────────────────────────────────────────────────────────────────────────────────────────┤ 
+│   DATE        : 10.13.2019												     			  │ 
+│   AUTHOR      : Paul Drangeid 															  │ 
+└─────────────────────────────────────────────────────────────────────────────────────────────┘ 
+ 
+#> 
+param (
+    [string]$credname,
+    [string]$credpath,
+    [string]$defaultuser,
+    [switch]$noui,
+    [int]$verbosity
+    )
+
+$scriptname=$($MyInvocation.MyCommand.Name)
+
+if ($noui -and $null -eq $verbosity){[int]$verbosity=0} #noui switch sets output verbosity level to 0 by default
+if ($null -eq $verbosity){[int]$verbosity=1} #verbosity level is 1 by default
+if (!($null -eq $credpath)){
+$credpath="HKCU:\Software\$credpath\$credname"
+$credpath=$credpath.replace('\\','\')
+}
+if ($null -eq $credpath){[string]$credpath="HKCU:\Software\Mycredentials\$credname"}
+if ($null -eq $defaultuser){[string]$defaultuser="Username"}
+
+Show-onscreen $("`nLoading includes: $PSScriptRoot\bg-sharedfunctions.ps1") 1
+Try{. "$PSScriptRoot\bg-sharedfunctions.ps1" | Out-Null}
+Catch{
+    Write-Warning "I wasn't able to load the sharedfunctions includes (which should live in the same directory as $global:srccmdline). `nWe are going to bail now, sorry 'bout that!"
+    Write-Host "Try running them manually, and see what error message is causing this to puke: $PSScriptRoot\bg-sharedfunctions.ps1"
+    Unregister-PSVars
+    BREAK
+    }
+
+    Add-Type -AssemblyName Microsoft.VisualBasic
+    AddRegPath $credpath
+    Get-Set-Credential $credname $credpath $($credname+"User") $($credname+"PW") $false $defaultuser
+    Ver-RegistryValue -RegPath $Path -Name $($credname+"User")
+    Get-SecurePassword $Path $($credname+"PW")
---
+++ update-modules.ps1
@@ -0,0 +1,131 @@
+<# 
+.SYNOPSIS 
+ Download & Update the agent with the latest versions from the repository
+ 
+ 
+.DESCRIPTION 
+ Setup and maintain agent scripts with latest version from the git repository.
+ 
+
+.NOTES 
+┌─────────────────────────────────────────────────────────────────────────────────────────────┐ 
+│ update-modules.ps1                                                                          │ 
+├─────────────────────────────────────────────────────────────────────────────────────────────┤ 
+│   DATE        : 10.12.2019 				               	                                  │ 
+│   AUTHOR      : Paul Drangeid 			                                                  │ 
+│   SITE        : https://github.com/pdrangeid/graph-commit                                   │ 
+└─────────────────────────────────────────────────────────────────────────────────────────────┘ 
+#> 
+
+$companyname="Blue Net Inc"
+$reporoot="https://raw.githubusercontent.com/pdrangeid"
+$path = $("$Env:Programfiles\$companyname")
+$localtz=Get-TimeZone | Select Id -ExpandProperty Id
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+function ConvertUTC {
+param($time, $fromTimeZone)
+  $oFromTimeZone = [System.TimeZoneInfo]::FindSystemTimeZoneById($fromTimeZone)
+  $utc = [System.TimeZoneInfo]::ConvertTimeToUtc($time, $oFromTimeZone)
+  return $utc
+} # End ConvertUTC
+
+
+function ConvertUTCtoLocal{
+param([String] $UTCTime)
+$strCurrentTimeZone = (Get-WmiObject win32_timezone).StandardName
+$TZ = [System.TimeZoneInfo]::FindSystemTimeZoneById($strCurrentTimeZone)
+$LocalTime = [System.TimeZoneInfo]::ConvertTimeFromUtc($UTCTime, $TZ)
+return $LocalTime
+} # End ConvertUTCtoLocal
+
+Function get-updatedgitfile([string]$reponame,[string]$repofile,[string]$localfilename){
+      # This function will query github API at the provided $reponame and $repofile and download if the $localfilename is older or missing.
+      # NOTE: unauthentication API queries to github (like this one) are rate-limited to 60 per hour (per IP address)
+      # So be sure you are only checking for a few files, and if it is a scheduled job, be sure you won't exceed this limit.
+      # You could add authenitcation to this function to avoid the 60/hr rate limitation.
+
+      $githuburl="https://api.github.com/repos/$reponame/commits?path=$repofile&page1&per_page=1"
+      Try{
+            $Restresult=(Invoke-RestMethod $githuburl -Method 'Get' -Headers @{Accept = "application/json"} -ErrorVariable RestError -ErrorAction SilentlyContinue -TimeoutSec 30)
+      }
+      Catch {
+            $ErrorMessage = $_.Exception.Message
+            if ($_.Exception.ItemName -like '*rate limit exceeded*') {
+                  Write-Warning "`nExceeded rate limit when querying github API: $githuburl"
+                  return $false
+            }
+
+            if ($ErrorMessage -eq 'Unable to connect to the remote server'){
+                  Write-Warning "`nUnable to connect to the remote server $githuburl"
+                  return $false
+            }
+            write-host "Error Message $ErrorMessage `nFailed Item:$_.Exception.ItemName `nhttp Response:$_.Exception.Response`n"
+                  return $false
+      }
+      #Get the date of the last commit for the repository file requested.
+      [datetime]$therepofiledate=$Restresult.commit[0].author.date | get-date -Format "yyyy-MM-ddTHH:mm:ss"
+      If (Test-Path -path $localfilename) {
+            $lastModifiedDate = (Get-Item $localfilename).LastWriteTime | get-date -Format "yyyy-MM-ddTHH:mm:ss"
+            $localfiletime = ConvertUTC $lastModifiedDate $localtz
+      if ($localfiletime -ge $therepofiledate){
+      }#end if (local file exists, and is the same or newer datestamp than that of the repository)
+      else {
+            write-host "$repofile will be updated..."
+            $downloadfile=$true
+      }#end else (file exists, but is older than the one in the repository)
+      
+      }#end if (the file DOES exists in the expected local path)
+
+      else {
+            $localtest="$repofile is not present and will be downloaded from the repository"
+            $downloadfile=$true
+      }# end else (local file doesn't exist in the expected local path)     
+      Write-host $localtest
+      
+            if ($downloadfile -eq $true) {
+            write-host "Downloading $repofile ($therepofiledate) from https://raw.githubusercontent.com/$reponame/master/$repofile"
+            $dlurl="https://raw.githubusercontent.com/$reponame/master/$repofile"
+            $client = new-object System.Net.WebClient
+            Try{
+            Write-Host "Gonna download $dlurl to $localfilename"
+            $dlresult = $client.DownloadFile($dlurl,$localfilename) 
+            }
+            Catch{
+            $ErrorMessage = $_.Exception.Message
+            $FailedItem = $_.Exception.ItemName
+            $error[0].Exception.ToString()
+            Write-Host $ErrorMessage
+            Write-Host $FailedItem
+            write-host "the error is "$error[0].Exception.ToString()
+            }
+      
+            $localtimestamp = ConvertUTCtoLocal $therepofiledate | get-date
+            #Convert the UTC of the repo file to the localtime, then set the local file's lastmodified property to the proper timestamp
+            Get-ChildItem  $localfilename | % {$_.LastWriteTime = $localtimestamp}
+            }
+      }# End Function get-updatedgitfile
+
+
+If(!(test-path $path))
+{
+      New-Item -ItemType Directory -Force -Path $path
+}
+
+$path = $("$Env:Programfiles\$companyname\Graph-Commit")
+If(!(test-path $path))
+{
+      New-Item -ItemType Directory -Force -Path $path
+}
+
+$rpath = "pdrangeid/graph-commit"
+get-updatedgitfile $rpath "set-n4jcredentials.ps1" "$path\set-regcredentials.ps1"
+get-updatedgitfile $rpath "set-n4jcredentials.ps1" "$path\update-modules.ps1"
+$rpath = "pdrangeid/bnwh-cache-agent"
+get-updatedgitfile $rpath "bg-sharedfunctions.ps1" "$path\bg-sharedfunctions.ps1"
+$rpath = "pdrangeid/n4j-pswrapper"
+get-updatedgitfile $rpath "get-cypher-results.ps1" "$path\get-cypher-results.ps1"
+
+
+exit
+
